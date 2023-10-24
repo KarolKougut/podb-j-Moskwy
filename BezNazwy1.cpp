bb7ce0541900ea809cb1bf130ebb5cec05b748db
KarolKougut <karolkogut3@gmail.com>
2023-10-24 11:07:19 +0200

    part 2

--- BezNazwy1.cpp
+++ BezNazwy1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
+	short zmienna = 10;
 	cout<<"Hello world";
+	cout<<endl;
+	cout<<"guwno nic nie zrobilem guwno";
+	cout<<zmienna;
 	return 0;
 }
