e6d386674264fc1d71911151d057a67512faf18e
KarolKougut <karolkogut3@gmail.com>
2023-10-30 11:59:45 +0100

    Komentarz

---
+++ 2-instrukcje-sterujace .cpp
@@ -0,0 +1,17 @@
+	#include<iostream>
+	using namespace std;
+	
+	int main(){
+
+		short a,b;
+		cout << "podaj a:  ";
+		cin >> a;
+		cout << "podaj a:  ";
+		cin >> b;
+		
+		if(a > b){
+			cout << "a > b" ;
+			}
+		else{
+			cout  << "a < b";}
+	}
